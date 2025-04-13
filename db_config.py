5a822b761d94f4f91269332852ebe62aa36d8368
vishesh-yadav-hear <visheshyadavhear@gmail.com>
2025-04-13 12:28:28 +0530

    Update db_config.py

---
+++ db_config.py
@@ -0,0 +1,8 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="sql12.freesqldatabase.com",
+    user="sql12772847",
+    password="tumhare_email_wali_password",
+    database="sql12772847"
+)
